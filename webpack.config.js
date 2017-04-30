60c95aabe6a8f884339ea3145f91a96da3f7a7b9
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-04-30 12:21:39 +0530

    Add less plugin to group media queries

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,55 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var LessPluginGroupMediaQueries = require('less-plugin-group-css-media-queries');
 
 module.exports = {
-	entry: './source/client.jsx',
-	output: {
-		path: path.resolve(__dirname, './assets'),
-		filename: 'scripts/feather.js'
-	},
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				exclude: /(node_modules|assets)/,
-				test: /\.jsx?$/,
-				use: 'eslint-loader'
-			}, {
-				exclude: /(node_modules|assets)/,
-				test: /\.jsx?$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['react', 'es2015', 'stage-1']
-					}
-				}
-			}, {
-				test: /\.less$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						'css-loader',
-						'postcss-loader',
-						'less-loader'
-					]
-				})
-			}
-		]
-	},
-	resolve: {
-		extensions: [".js", ".json", ".jsx", ".css"]
-	},
-	plugins: [
-		new ExtractTextPlugin({
-			filename: 'styles/feather.css',
-			allChunks: true
-		})
-	],
-	devtool: 'source-map',
-  	watch: true
+  entry: './source/client.jsx',
+  output: {
+    path: path.resolve(__dirname, './assets'),
+    filename: 'scripts/feather.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        exclude: /(node_modules|assets)/,
+        test: /\.jsx?$/,
+        use: 'eslint-loader'
+      }, {
+        exclude: /(node_modules|assets)/,
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-1']
+          }
+        }
+      }, {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader', {
+              loader: "less-loader",
+              options: {
+                plugins: [ LessPluginGroupMediaQueries ]
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'styles/feather.css',
+      allChunks: true
+    })
+  ],
+  devtool: 'source-map',
+  watch: true
 }
