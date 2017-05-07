19bbf44dfa7e02c4ff43bcc97bfd68ab717b8f0a
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-05-07 23:20:08 +0530

    + new action FETCH_HOME_CONTENTS

--- source/actions/index.js
+++ source/actions/index.js
@@ -1,10 +1,39 @@
 /* global ghost */
 import axios from 'axios';
 
+export const FETCH_HOME_CONTENTS = 'FETCH_HOME_CONTENTS';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 export const LOADING = 'LOADING';
 export const API_ERROR = 'API_ERROR';
+
+export function fetchHomeContents() {
+  const userDetails = axios.get(ghost.url.api('users', {
+    fields: 'bio,image,name',
+    limit: 1
+  }));
+  const initialPosts = axios.get(ghost.url.api('posts', {
+    include: 'tags',
+    fields: 'id,title,slug,image,tags,html',
+    limit: 5
+  }));
+  return (dispatch) => {
+    dispatch({
+      type: LOADING,
+      payload: { kind: FETCH_HOME_CONTENTS }
+    });
+    Promise.all([userDetails, initialPosts]).then(responses => dispatch({
+      type: FETCH_HOME_CONTENTS,
+      payload: {
+        user: responses[0].data.users[0],
+        posts: responses[1].data.posts
+      }
+    })).catch(error => dispatch({
+      type: API_ERROR,
+      payload: { kind: FETCH_HOME_CONTENTS, error }
+    }));
+  };
+}
 
 export function fetchPosts(limit = '5') {
   const request = axios.get(ghost.url.api('posts', { limit }));
