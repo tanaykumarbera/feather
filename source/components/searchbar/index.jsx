d8c0616aae38ec2bf14b8c72fa3203e55d7caddf
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-06-25 15:11:45 +0530

    -- onclick from Searchbar component.

--- source/components/searchbar/index.jsx
+++ source/components/searchbar/index.jsx
@@ -5,15 +5,14 @@ import { IconFont } from '../../utils';
 import './searchbar.less';
 
 const SearchBar = props => (
-  <button className={`f-searchbar ${props.theme}`} onClick={props.onClick}>
+  <button className={`f-searchbar ${props.theme}`} >
     What&apos;s in your mind?
     <FIcon icon={IconFont.SEARCH} />
   </button>
 );
 
 SearchBar.propTypes = {
-  theme: PropTypes.string,
-  onClick: PropTypes.func.isRequired
+  theme: PropTypes.string
 };
 
 SearchBar.defaultProps = {
