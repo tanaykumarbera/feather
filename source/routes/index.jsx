1cc6355dbb4cd3307eec3e2b743debcf329d9193
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-07-21 01:35:48 +0530

    + React router Switch :: So that only first matching component is rendered

--- source/routes/index.jsx
+++ source/routes/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import FHome from '../containers/home';
 import FList from '../containers/list';
@@ -10,10 +10,12 @@ const FRouter = () => {
   return (
     <BrowserRouter forceRefresh={!supportsHistory}>
       <div className="feather-wrap">
-        <Route exact path="/" component={FHome} />
-        <Route path="/tag/:slug" component={FList} />
-        <Route path="/posts" component={FList} />
-        <Route path="/:slug" component={FPost} />
+        <Switch>
+          <Route exact path="/" component={FHome} />
+          <Route path="/tag/:slug" component={FList} />
+          <Route path="/posts" component={FList} />
+          <Route path="/:slug" component={FPost} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
