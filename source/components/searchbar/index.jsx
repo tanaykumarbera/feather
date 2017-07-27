17af1c4e32425a12f57df6ac9fd7498cdccf9231
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-07-27 19:40:14 +0530

    Search component. Need to integrate with Google custum search later

--- source/components/searchbar/index.jsx
+++ source/components/searchbar/index.jsx
@@ -5,13 +5,14 @@ import { IconFont } from '../../utils';
 import './searchbar.less';
 
 const SearchBar = props => (
-  <button className={`f-searchbar ${props.theme}`} >
+  <button className={`f-searchbar ${props.theme}`} onClick={props.onClick} >
     What&apos;s in your mind?
     <FIcon icon={IconFont.SEARCH} />
   </button>
 );
 
 SearchBar.propTypes = {
+  onClick: PropTypes.func.isRequired,
   theme: PropTypes.string
 };
 
