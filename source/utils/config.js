cafc1b6ded6e0cb70d18335d35f53255df71fc96
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-07-30 19:47:34 +0530

    Add urls to icon links

---
+++ source/utils/config.js
@@ -0,0 +1,9 @@
+const Config = {
+  URL_REPO: 'https://github.com/tanaykumarbera/feather',
+  URL_GITHUB: 'https://github.com/tanaykumarbera',
+  URL_TWITTER: 'https://twitter.com/tanaykumarbera',
+  URL_INSTAGRAM: 'https://www.instagram.com/tanaykumarbera',
+  URL_HOME: '/'
+};
+
+export default Config;
