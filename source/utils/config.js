92cd5c15d204fa6e3f0149fbdd5aaa93d90c7e2b
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-08-02 00:40:37 +0530

    Config, ++ BLOG_AUTHOR, PUBLISHER, EMAIL details

--- source/utils/config.js
+++ source/utils/config.js
@@ -1,5 +1,8 @@
 const Config = {
   BLOG_TITLE: 'blog.tanay.co',
+  BLOG_PUBLISHER: 'tanay.co',
+  BLOG_AUTHOR_NAME: 'Tanay Kumar Bera',
+  BLOG_AUTHOR_EMAIL: 'tanaykumarbera@gmail.com',
   URL_REPO: 'https://github.com/tanaykumarbera/feather',
   URL_GITHUB: 'https://github.com/tanaykumarbera',
   URL_TWITTER: 'https://twitter.com/tanaykumarbera',
