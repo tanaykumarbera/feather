251fe110502b567b58cf6a2b44adbcbfc0f17369
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2017-08-10 13:04:08 +0530

    fetchHomeContents :: ++ feature_image

--- source/actions/index.js
+++ source/actions/index.js
@@ -14,7 +14,7 @@ export function fetchHomeContents() {
   }));
   const initialPosts = axios.get(ghost.url.api('posts', {
     include: 'tags',
-    fields: 'id,title,slug,image,tags,html',
+    fields: 'id,title,slug,image,feature_image,tags,html',
     limit: 5
   }));
   return (dispatch) => {
